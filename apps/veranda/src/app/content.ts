328261f6d82b8e0e035fbe258934841188fc1d30
Egor Sasin <egorsasin@gmail.com>
2025-10-24 17:05:08 +0200

    add context

---
+++ apps/veranda/src/app/content.ts
@@ -0,0 +1,17 @@
+import { Component, inject } from '@angular/core';
+
+// ИМПОРТ КОНТЕКСТА ИЗ MFE SHELL
+import { MY_TOKEN } from 'shell/Wrapper';
+
+@Component({
+  imports: [],
+  selector: 'vsk-context',
+  template: `
+    <h1>VERANDA APP</h1>
+
+    <div style="color: red; padding: 1rem;">{{ myToken }}</div>
+  `,
+})
+export class VerandaContext {
+  protected myToken = inject(MY_TOKEN);
+}
