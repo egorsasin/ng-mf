9c1bdf1f5f49b02e60ebfc6cdbc9da7b6f94fea8
Egor Sasin <egorsasin@gmail.com>
2025-10-24 17:03:02 +0200

    update dependencies

--- apps/shell/src/app/app.component.ts
+++ apps/shell/src/app/app.component.ts
@@ -4,6 +4,6 @@ import { RemoteEntry } from './remote-entry/entry';
 @Component({
   imports: [RemoteEntry],
   selector: 'vsk-app',
-  template: '<vsk-shell>Shell App</vsk-shell>',
+  template: '<vsk-shell><h1>MAIN APPLICATION</h1></vsk-shell>',
 })
 export class AppComponent {}
